e538fbca738eb01eb1449307f684a1059946d411
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-19 08:47:59 -0500

    Select de origin de registro con handlebars

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,11 +1,22 @@
 $( document ).ready( function() {
   var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -11.729697, lng: -75.28832},
-      zoom: 10
-    });
-    var marker = new google.maps.Marker({
-      position: {lat: parseFloat(-12.084824), lng: parseFloat(-76.971121)},
-      map: map,
-      title: 'Hello World!'
-    });
+    center: {lat: -11.729697, lng: -75.28832},
+    zoom: 10
+  });
+  var marker = new google.maps.Marker({
+    position: {lat: parseFloat(-12.084824), lng: parseFloat(-76.971121)},
+    map: map,
+    title: 'Hello World!'
+  });
+
+  //select de relacion con la Universidad
+  var source   = document.getElementById("hb-form-group-relacion").innerHTML;
+  var template = Handlebars.compile(source);
+  var context = [
+    {id: 1, nombre: "Alumno/Exalumno"},
+    {id: 2, nombre: "Empleado"},
+    {id: 3, nombre: "Externo"},
+  ];
+  var template_compiled = template(context);
+  $("#form-group-relacion").html(template_compiled);
 });
