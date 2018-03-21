b26fe390df11c913e8fa67ed0f91028bcfd78d12
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-21 12:03:10 -0500

    Tablas de participantes de eventos con botones

--- views/_table_empleado.js
+++ views/_table_empleado.js
@@ -1,5 +1,5 @@
 var empleado_array_json_th = [
-	{titulo:"id", index: "_id", estilos:"width: 10px; display:none;"},
+	{titulo:"id", index: "id", estilos:"width: 10px; display:none;"},
   {titulo:"Código",index:"codigo",estilos:"width: 70px;"},
   {titulo:"DNI",index:"dni",estilos:"width: 70px;"},
 	{titulo:"Apellido Paterno",index:"paterno",estilos:"width: 120px;"},
@@ -8,24 +8,25 @@ var empleado_array_json_th = [
   {titulo:"Correo Empleado",index:"correo_laboral",estilos:"width: 120px;"},
   {titulo:"Correo Personal",index:"correo_personal",estilos:"width: 120px;"},
   {titulo:"Celuluar",index:"celular",estilos:"width: 90px;"},
-	{titulo:"Botones",index:"NA",estilos:"width: 80px;"}
+	{titulo:"Botones",index:"NA",estilos:"width: 40px; padding-left: 0px;"}
 ];
 
 var empleado_array_json_td = [
-	{tipo:"label_id",estilos:"color: blue; display:none", index:"_id", edicion:""},
-  {tipo:"text",estilos:"width:70px;", index:"codigo", edicion:""},
-  {tipo:"text",estilos:"width:70px;", index:"dni", edicion:""},
-	{tipo:"text",estilos:"width:120px;", index:"paterno", edicion:""},
-  {tipo:"text",estilos:"width:120px;", index:"materno", edicion:""},
-  {tipo:"text",estilos:"width:100px;", index:"nombres", edicion:""},
-  {tipo:"text",estilos:"width:120px;", index:"correo_laboral", edicion:""},
-  {tipo:"text",estilos:"width:120px;", index:"correo_personal", edicion:""},
-  {tipo:"text",estilos:"width:120px;", index:"celular", edicion:""},
+	{tipo:"label_id",estilos:"color: blue; display:none", index:"id", edicion:""},
+  {tipo:"label",estilos:"width:70px;", index:"codigo", edicion:""},
+  {tipo:"label",estilos:"width:70px;", index:"dni", edicion:""},
+	{tipo:"label",estilos:"width:120px;", index:"paterno", edicion:""},
+  {tipo:"label",estilos:"width:120px;", index:"materno", edicion:""},
+  {tipo:"label",estilos:"width:100px;", index:"nombres", edicion:""},
+  {tipo:"label",estilos:"width:120px;", index:"correo_laboral", edicion:""},
+  {tipo:"label",estilos:"width:120px;", index:"correo_personal", edicion:""},
+  {tipo:"label",estilos:"width:120px;", index:"celular", edicion:""},
 	{tipo:"botones", index:"botones", edicion:"true"}
 ];
 
 var empleado_array_json_btn_td = [
-	{clase:"fa fa-chevron-right", href:"#/accesos/sistemas/menu/", alt:"Gestionar Menú",estilos:"padding-left: 2px;", tipo:"btn_td_href", operacion:"IrURL"},
+	{clase:"fa fa-search", href:"#/empleado/ver/", alt:"Ver Empleado",estilos:"padding-left: 5px;", tipo:"btn_td_href", operacion:"IrURL"},
+	{clase:"fa fa-list-ul", href:"#/empleado/participacion/", alt:"Participación en Eventos",estilos:"padding-left: 5px;", tipo:"btn_td_href", operacion:"IrURL"},
 ];
 
 var empleado_array_json_btn = [
--- views/_table_externo.js
+++ views/_table_externo.js
@@ -6,22 +6,24 @@ var externo_array_json_th = [
   {titulo:"Nombre",index:"nombres",estilos:"width: 100px;"},
   {titulo:"Correo Personal",index:"correo",estilos:"width: 120px;"},
   {titulo:"Teléfono",index:"telefono",estilos:"width: 90px;"},
-	{titulo:"Botones",index:"NA",estilos:"width: 80px;"}
+	{titulo:"Botones",index:"NA",estilos:"width: 60px; padding-left: 5px;"}
 ];
 
 var externo_array_json_td = [
 	{tipo:"label_id",estilos:"color: blue; display:none", index:"_id", edicion:""},
-  {tipo:"text",estilos:"width:70px;", index:"dni", edicion:""},
-	{tipo:"text",estilos:"width:120px;", index:"paterno", edicion:""},
-  {tipo:"text",estilos:"width:120px;", index:"materno", edicion:""},
-  {tipo:"text",estilos:"width:100px;", index:"nombres", edicion:""},
-  {tipo:"text",estilos:"width:120px;", index:"correo", edicion:""},
-  {tipo:"text",estilos:"width:120px;", index:"telefono", edicion:""},
+  {tipo:"label",estilos:"width:70px;", index:"dni", edicion:""},
+	{tipo:"label",estilos:"width:120px;", index:"paterno", edicion:""},
+  {tipo:"label",estilos:"width:120px;", index:"materno", edicion:""},
+  {tipo:"label",estilos:"width:100px;", index:"nombres", edicion:""},
+  {tipo:"label",estilos:"width:120px;", index:"correo", edicion:""},
+  {tipo:"label",estilos:"width:120px;", index:"telefono", edicion:""},
 	{tipo:"botones", index:"botones", edicion:"true"}
 ];
 
 var externo_array_json_btn_td = [
-	{clase:"fa fa-chevron-right", href:"#/accesos/sistemas/menu/", alt:"Gestionar Menú",estilos:"padding-left: 2px;", tipo:"btn_td_href", operacion:"IrURL"},
+	{clase:"fa fa-pencil", href:"#/externo/editar/", alt:"Editar Externo",estilos:"padding-left: 5px;", tipo:"btn_td_href", operacion:"IrURL"},
+	{clase:"fa fa-search", href:"#/externo/ver/", alt:"Ver Externo",estilos:"padding-left: 5px;", tipo:"btn_td_href", operacion:"IrURL"},
+	{clase:"fa fa-list-ul", href:"#/externo/participacion/", alt:"Participación en Eventos",estilos:"padding-left: 5px;", tipo:"btn_td_href", operacion:"IrURL"},
 ];
 
 var externo_array_json_btn = [
